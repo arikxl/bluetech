990d65996c96e531f425e6ae281b8a5d5d5426b2
Arik Alexandrov <arikxl@gmail.com>
2023-08-31 18:25:12 +0300

    args

---
+++ api.py
@@ -0,0 +1,11 @@
+import requests
+
+req = requests.get("https://swapi.dev/api/people/55/")
+print(req.status_code)
+person = req.json()
+print(person['name'])
+print('films involved in: ')
+for film in person['films']:
+    req = requests.get(film)
+    film = req.json()
+    print('Film is: ', film['title'])
---
+++ args.py
@@ -0,0 +1,6 @@
+def add_nums(*args):
+    total =0
+    for ar in args:
+        total +=ar
+    print(total)
+add_nums(1,2,3,4,5)
